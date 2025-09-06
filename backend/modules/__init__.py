bb399db5b6940207e8d96fb637c48e4e16b64d3a
ShardulAgg <shardulagg@convosearch.com>
2025-09-06 16:14:27 -0700

    movie generation

--- backend/modules/__init__.py
+++ backend/modules/__init__.py
@@ -1,4 +1,4 @@
 from .comic_generator import generate_comic
-from .movie_generator import generate_movie
+from .movie_generator import generate_movie_from_panels
 
-__all__ = ["generate_comic", "generate_movie"]
+__all__ = ["generate_comic", "generate_movie_from_panels"]
